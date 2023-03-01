0b0c8ed8d6001d232b3cd91f63ff434f5972e1cc
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-03-01 22:21:16 +0100

    move converter

--- ConferenceApp/src/converter/BoolToVisibilityConverter.cs
+++ ConferenceApp/src/converter/BoolToVisibilityConverter.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Data;
 
-namespace ConferenceApp.view.dialog
+namespace ConferenceApp.converter
 {
 	public class BoolToVisibilityConverter : IValueConverter
 	{
