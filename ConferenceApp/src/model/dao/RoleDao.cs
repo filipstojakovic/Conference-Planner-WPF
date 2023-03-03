e5cff4517f3e1cbedcaba39d9bf8c7c6658c1943
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-03-03 09:57:50 +0100

    finish user dialog

--- ConferenceApp/src/model/dao/RoleDao.cs
+++ ConferenceApp/src/model/dao/RoleDao.cs
@@ -1,60 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 using ConferenceApp.model.entity;
 using MySql.Data.MySqlClient;
 
 
 namespace ConferenceApp.model.dao
 {
-	public class RoleDao : BaseDao
-	{
-		public Role findByName(string roleName)
-		{
-			const string sql = @"
+    public class RoleDao : BaseDao
+    {
+        public Role findByName(string roleName)
+        {
+            const string sql = @"
 				SELECT * FROM role
 				WHERE name = @name";
 
-			Role role = null;
-			using (var command = new MySqlCommand(sql, connection))
-			{
-				command.Parameters.AddWithValue("@name", roleName);
-				using var reader = command.ExecuteReader();
-				while (reader.Read())
-				{
-					role = new Role
-					{
-						Id = reader.GetInt32(reader.GetOrdinal("id")),
-						Name = reader.GetString(reader.GetOrdinal("name")),
-					};
-				}
-			}
-			return role;
-		}
+            Role role = null;
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@name", roleName);
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    role = new Role
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("id")),
+                        Name = reader.GetString(reader.GetOrdinal("name")),
+                    };
+                }
+            }
 
-		public Role insert(string roleName)
-		{
-			const string sql = "INSERT INTO role(name) VALUES(@name)";
+            return role;
+        }
 
-			Role role = null;
-			MySqlTransaction transaction = connection.BeginTransaction();
-			try
-			{
-				using (var command = new MySqlCommand(sql, connection, transaction))
-				{
-					command.Parameters.AddWithValue("@name", roleName);
-					command.ExecuteNonQuery();
-					int roleId = (int)command.LastInsertedId;
-					role = new Role { Id = roleId, Name = roleName };
-				}
+        public List<Role> findAll()
+        {
+            const string sql = "SELECT * FROM role";
 
-				transaction.Commit();
-			}
-			catch (Exception)
-			{
-				transaction.Rollback();
-			}
+            List<Role> roles = new List<Role>();
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Role role = new Role
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("id")),
+                        Name = reader.GetString(reader.GetOrdinal("name")),
+                    };
+                    roles.Add(role);
+                }
+            }
 
-			return role;
+            return roles;
+        }
 
-		}
-	}
+        public Role insert(string roleName)
+        {
+            const string sql = "INSERT INTO role(name) VALUES(@name)";
+
+            Role role = null;
+            MySqlTransaction transaction = connection.BeginTransaction();
+            try
+            {
+                using (var command = new MySqlCommand(sql, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@name", roleName);
+                    command.ExecuteNonQuery();
+                    int roleId = (int)command.LastInsertedId;
+                    role = new Role { Id = roleId, Name = roleName };
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+            }
+
+            return role;
+        }
+    }
 }
--- ConferenceApp/src/view/dialog/UserDialog.xaml.cs
+++ ConferenceApp/src/view/dialog/UserDialog.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using ConferenceApp.model;
+using ConferenceApp.model.dao;
 using ConferenceApp.model.entity;
 
 namespace ConferenceApp.view.dialog
 {
     public partial class UserDialog : Window
     {
+        private readonly RoleDao roleDao;
         public User UserDialogData { get; set; }
+        public Role SelectedRole { get; set; }
         public bool Edit { get; set; }
+
+        public List<Role> AllRoles { get; set; }
 
         public UserDialog(User user = null, bool edit = true)
         {
             InitializeComponent();
             Edit = edit;
             this.UserDialogData = new User(user);
-            DataContext = UserDialogData;
             Button.Content = edit ? "Edit" : "Create";
+            this.Title = (edit ? "Edit" : "Create") + " user";
+
+            Role userRole = UserDialogData.Roles
+                .Where(role => role.Name.ToLower() == "user")
+                .FirstOrDefault();
+            SelectedRole = UserDialogData.Roles
+                .Where(role => role.Name.ToLower() != "user")
+                .DefaultIfEmpty(userRole)
+                .FirstOrDefault();
+
+            roleDao = new RoleDao();
+            AllRoles = roleDao.findAll();
+            DataContext = UserDialogData;
+            RoleComboBox.DataContext = this;
+            RoleComboBox.ItemsSource = AllRoles;
+
             // editBtn.Content = edit ? "Edit" : "Create";
             // editBtn.DataContext = this;
         }
@@ -27,6 +51,12 @@ namespace ConferenceApp.view.dialog
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            UserDialogData.Roles = new BindingList<Role> { SelectedRole };
+            if (SelectedRole.Name.ToLower() != "user")
+            {
+                UserDialogData.Roles.Add(roleDao.findByName(UserRoleEnum.user.ToString()));
+            }
+
             DialogResult = true;
             Close();
         }
