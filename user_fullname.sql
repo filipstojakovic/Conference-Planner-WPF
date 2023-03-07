6f8b311ac0745b23e1cae70b0e9d23b065b131e6
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-03-07 15:37:48 +0100

    procedure. view, trigger

---
+++ user_fullname.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE `get_user_fullname` (IN userId int, OUT full_name varchar(255))
+BEGIN
+	DECLARE FirstName VARCHAR(50);
+    DECLARE LastName VARCHAR(50);
+    
+SELECT 
+    first_name, last_name
+INTO FirstName , LastName FROM
+    user
+WHERE
+    id = userId;
+    
+    SET full_name = CONCAT(FirstName, ' ', LastName);
+END
